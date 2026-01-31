0920c9b2a6a16da3443c36bac4aa4bfc1ba98879
aaronsilinskas <aaron@mindwidgets.com>
2026-01-31 20:54:56 +0900

    Add Regen spell

---
+++ src/aura/spell/elemental/regen.py
@@ -0,0 +1,24 @@
+from aura.aura import Aura, Spell, SpellTags
+from aura.spell.elemental.elements import ElementTags
+from aura.values import Duration
+
+
+class RegenSpell(Spell):
+    """A spell that provides magic regeneration over time."""
+
+    def __init__(self, regen_rate: float, duration: float):
+        """Initialize a RegenSpell.
+
+        Args:
+            regen_rate: The rate of magic regeneration per second.
+            duration: The duration of the spell in seconds.
+        """
+        super().__init__(tags=[SpellTags.BUFF, ElementTags.WATER])
+        self.regen_rate = regen_rate
+        self.duration = Duration(duration)
+
+    def update(self, aura: Aura, elapsed_time: float) -> bool:
+        regen_amount = self.regen_rate * min(elapsed_time, self.duration.remaining)
+        aura.magic.value += regen_amount
+
+        return self.duration.update(elapsed_time)
