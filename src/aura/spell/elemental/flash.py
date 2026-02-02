a16f0ead36549cd5b8e74ccb5234a54bdeb144fe
aaronsilinskas <aaron@mindwidgets.com>
2026-02-02 17:15:21 +0900

    Add Flash spell

---
+++ src/aura/spell/elemental/flash.py
@@ -0,0 +1,20 @@
+from aura.aura import Aura, Spell, SpellTags
+from aura.spell.elemental.elements import ElementTags
+from aura.values import Duration
+
+
+class FlashSpell(Spell):
+    """A spell that temporarily turns all visual indicators white until the duration expires."""
+
+    def __init__(self, duration: float) -> None:
+        super().__init__([SpellTags.DEBUFF, ElementTags.LIGHT])
+        self._base_duration = duration
+        self.duration = Duration(duration)  # in seconds
+
+    def update(self, aura: Aura, elapsed_time: float) -> bool:
+        return self.duration.update(elapsed_time)
+
+    def _update_level(self, level: int) -> None:
+        self.duration.length = Spell.LEVEL_SCALER.scale_value(
+            self._base_duration, level
+        )
